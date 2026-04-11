a787b4f8c09494c3f31167dd3297da26576e19bf
GitHub Action <action@github.com>
2026-04-11 08:03:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/leonard2005n/aicodeexplain/services/MyProjectService.kt
+++ src/main/kotlin/com/github/leonard2005n/aicodeexplain/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.leonard2005n.aicodeexplain.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leonard2005n.aicodeexplain.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/leonard2005n/aicodeexplain/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/leonard2005n/aicodeexplain/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.leonard2005n.aicodeexplain.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
